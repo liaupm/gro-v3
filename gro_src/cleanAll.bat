fcaa8b087d9c53ac218577934befdaa84fba619c
elenube <154708190+elenube@users.noreply.github.com>
2024-02-25 01:05:15 +0100

    Add files via upload

---
+++ gro_src/cleanAll.bat
@@ -0,0 +1,11 @@
+rmdir /s /q ccl_Release
+rmdir /s /q ccl_Debug
+rmdir /s /q gro_Release
+rmdir /s /q gro_Debug
+
+md ccl_Release
+md ccl_Debug
+md gro_Release
+md gro_Debug
+
+Echo 
---
+++ gro_src/m.bat
@@ -0,0 +1,15 @@
+cd Utility
+rmdir /s /q build\win\release
+md build\win\release
+make MODE=release -B
+cd ..
+cd CellPro
+rmdir /s /q build\win\release
+md build\win\release
+make MODE=release -B
+cd ..
+cd cellsignals
+rmdir /s /q build\win\release
+md build\win\release
+make MODE=release -B
+cd ..
